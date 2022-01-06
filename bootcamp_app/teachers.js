c0f1ed9fb84fac6b8260e8a52f6ce8ea95b08787
joshuavottero <joshuavottero@outlook.com>
2022-01-06 03:47:24 +0000

    added SQL INJECTION safety to students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2] || "JUL02";
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`]; 
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id  = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`).then(res => {
+`;
+
+pool.query(queryString, values).then(res => {
   console.log("connected");
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
